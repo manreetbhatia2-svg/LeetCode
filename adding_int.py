abe8439d443f34c3fd12c49dca42313475bad046
manreetbhatia2-svg <manreetbhatia2@gmail.com>
2026-01-18 15:52:26 +0530

    adding the numbers

---
+++ adding_int.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def sum(self, num1, num2):
+        if 100>=num1>=-100 and 100>=num2>=-100 :
+            return (num1 + num2)
+answer = Solution()
+print(answer.sum(5,10))
