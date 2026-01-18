8239c6086c1df9694d2d8744a5c300428599d911
manreetbhatia2-svg <manreetbhatia2@gmail.com>
2026-01-18 16:13:32 +0530

    adding a new comment

--- adding_int.py
+++ adding_int.py
@@ -2,5 +2,7 @@ class Solution(object):
     def sum(self, num1, num2):
         if 100>=num1>=-100 and 100>=num2>=-100 :
             return (num1 + num2)
+
+# creating an object
 answer = Solution()
 print(answer.sum(5,10))
