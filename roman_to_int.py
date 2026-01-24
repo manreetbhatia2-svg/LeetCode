20123270d44e9c4a4233f4f03451049435f09381
rashmeet bhatia <manreetbhatia2@gmail.com>
2026-01-24 21:12:56 +0530

    roman to integer

--- roman_to_int.py
+++ roman_to_int.py
@@ -7,7 +7,7 @@ roman = {'I':1, 'V':5, 'X':10,'L':50,'C':100,'D':500,'M':1000}
 for i in range (len(s)):
   s[i] = roman[s[i]]
 
-# Converting roman to integer using addition and subtraction
+# Converting roman to integer using addition and subtraction 
 i = 0
 Int  = 0
 while 0<= i < len(s):
