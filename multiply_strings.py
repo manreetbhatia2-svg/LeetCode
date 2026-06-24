7a7c3965133e91f1fae26ecd0dd83e49dd9da126
manreetbhatia2-svg <manreetbhatia2@gmail.com>
2026-06-24 21:03:28 +0530

    multiplying_strings

--- multiply_strings.py
+++ multiply_strings.py
@@ -1,18 +1,18 @@
+'''Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2, also represented as a string.
+Note: You must not use any built-in BigInteger library or convert the inputs to integer directly.'''
 class Solution(object):
     def multiply(self, num1, num2):
-        if num1 == "0" or num2 == "0":
-            return "0"
-        final_ans = ''
-        result = [0]* (len(num1) + len(num2))  # as max len of 2 no.s being multiplied is sum of len of individual no.
-        
-        for i in range(len(num2)-1,-1,-1):
-            for j in range(len(num1)-1,-1,-1):
-                product = int(num2[i]) * int(num1[j])
-                total = product + result[i+j+1]
-                result[i+j+1] = total%10
-                result[i+j] += total//10
-                        
-        return("".join(map(str,result)).lstrip("0"))
+        answer = 0
+        place1 = 1
+        for i in num1[::-1]:
+            place2 = 1
+            for j in num2[::-1]:
+                answer += place1 * int(i) * place2 * int(j)
+                place2 = place2*10
+            place1 = place1*10
 
+    
+        return str(answer)
+    
 sol = Solution()
-print(sol.multiply("2","0"))
+print(sol.multiply('12','13'))
